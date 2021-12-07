e8537792f420c97446567130062eae1f9d5f0656
TURKCELL\EXT02D48072 <kubra.erbilen@gmail.com>
2021-12-07 12:44:15 +0300

    e mail opsiyonu eklend.

--- EmployeeManagement.Common/ConstantsModels/ResultConstant.cs
+++ EmployeeManagement.Common/ConstantsModels/ResultConstant.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmployeeManagement.Common.ConstantsModels
+﻿namespace EmployeeManagement.Common.ConstantsModels
 {
     public static class ResultConstant
     {
-        public const string RecordFound = "Kayıt Bulundu.";
-        public const string RecordNotFound = "Kayıt Bulunamadı.";
+        public const string RecordFound = "Kayıt Bulundu";
+        public const string RecordNotFound = "Kayıt Bulunamadı";
+        public const string RecordCreateSuccessfully = "Kayıt Başarıyla Gerçekleşti";
+        public const string RecordCreateNotSuccessfully = "Kayıt Gerçekleştirirken Hata Oluştu";
+        public const string RecordRemoveNotSuccessfully = "Silme Gerçekleştirirken Hata Oluştu";
+        public const string RecordRemoveSuccessfully = "Silme Gerçekleştirildi";
+
+        //---------------------------------------------------------------------------------------//
+        public const string Admin_Role = "Administrator";
+        public const string Employee_Role = "Employee";
+
+        public const string Admin_Email = "kubra.erbilen@gmail.com";
+        public const string Admin_Password = "Admin123!";
+        //---------------------------------------------------------------------------------------//
+        public const string LoginUserInfo = "Giriş Yapan Kullanıcı Bilgisi";
+        //---------------------------------------------------------------------------------------//
+        public const string MailTagHelperSuffix = "noktaatisi.com";
     }
 }
